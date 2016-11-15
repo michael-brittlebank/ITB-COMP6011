3029d163e55b7ffa8b4e59beb88d1e4dc6e757f3
mike-stumpf <mike.a.stumpf@gmail.com>
2016-11-15 14:33:00 +0000

    core - added main.java file

---
+++ main.java
@@ -0,0 +1,13 @@
+/**************************************************/
+/* Assignemnt 1 */
+/* Written by: Mike Stumpf */
+/* Course: COMP H6011 */
+/* Language: Java */
+/* Date Started: November 15, 2016 */
+/* Last Update: December 12, 2016 */
+/* Program Description: A banking institution */
+/* requires a simple program for processing and analysing */
+/* account holders’ details. For simplicity and ease of */
+/* testing reasons, the bank has a relatively small number */
+/* of account holders (maximum of 20).  */
+/**************************************************/
